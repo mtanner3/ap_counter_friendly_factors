a854339d273d4772b3c5a665e6c016dca30efdaf
Matt Tanner <mtanner3@gmail.com>
2016-08-14 02:43:02 -0600

    updated test harness to run the file of primes

--- test_harness.py
+++ test_harness.py
@@ -1,9 +1,14 @@
 
-import find_friendly_multiplicands as ffm
+import counter_factors as cf
 
 FH = open ("primes_from_2053_to_4194301.txt")
 for line in FH.readlines():
     target = int(line)
-    res, err = ffm.find_friendly_multiplicands(target)
-    print "target %d => %d * %d = %d %d" % (target, res[0], res[1], res[0]*res[1], err) 
+    factors = cf.get_counter_factors(target)
+    if factors is None:
+        print "No solution found for %d" % target
+        continue
+    product = factors[0] * factors[1]
+    print "target %7d f1 %4d f2 %4d product %7d off %d" % (target, factors[0], factors[1], product, target - product)
+
 
